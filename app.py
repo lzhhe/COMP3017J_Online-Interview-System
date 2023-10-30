80a538566e3101c8d6b4839c1865216bcd572223
MysticMosaic <2046777320@qq.com>
2023-10-30 19:49:42 +0800

    new home.html login.html

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+from flask import render_template
+
 from App import create_app
 
 app = create_app()
+
+
+# Define a route for testing purposes
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+# Define a route for testing purposes
+@app.route('/')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
