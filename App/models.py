77379409d04c3ecc47ed9acdd3c952c2e2c7ad65
lzhhe <67408547+lzhhe@users.noreply.github.com>
2023-10-31 20:11:44 +0800

    modify on login

--- App/models.py
+++ App/models.py
@@ -1,35 +1,31 @@
 from .extents import db
 
-
 class User(db.Model):
     __tablename__ = 'user'
-    UID = db.Column(db.Integer, primary_key=True, autoincrement=True)  # id唯一
-    username = db.Column(db.String(30), unique=True, nullable=False)  # 用户名
-    password = db.Column(db.String(256), nullable=False)  # 密码
-    email = db.Column(db.String(30), nullable=False)  # 邮箱
-    status = db.Column(db.Integer, nullable=False, default=2)  # 0是管理员，1是面试官，2是应试者
+    UID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    status = db.Column(db.Integer, nullable=False, default=2)
 
+class Position(db.Model):
+    __tablename__ = 'position'
+    PID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    positionName = db.Column(db.String(255), nullable=False)
+    positionDescription = db.Column(db.String(1000), nullable=False)
+    positionRequirement = db.Column(db.String(1000), nullable=False)
+    salary = db.Column(db.Integer, nullable=False)
 
-# class position(db.Model):
-#     __table__ = 'position'
-#     PID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     positionName = db.Column(db.String(255), nullable=False)  # like java developer, architect
-#     positionDescription = db.Column(db.String(1000), nullable=False)  # description
-#     positionRequirement = db.Column(db.String(1000), nullable=False)  # job content
-#     salary = db.Column(db.Integer, nullable=False)
-#
-#
-# class interviewResult(db.Model): # to record the result of a interview
-#     __table__name = 'interviewResult'
-#     UID = db.Column(db.Integer, primary_key=True, autoincrement=True) # the user ID who is interviewed
-#     answer = db.Column(db.Text)
-#     grade = db.Column(db.Integer, nullable=False) # it is given by the examiner
-#     evaluation = db.db.Column(db.Text, nullable=False)
-#     status = db.db.Column(db.Integer, nullable=False)  # 1 pass, 2 reject, 3 alternate
-#
-#
-# class applicaiton(db.Model):
-#     __table__name = 'application'
-#     UID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     PID = db.Column(db.Integer, primary_key=True, autoincrement=True) # apply job ID
-#     salary = db.Column(db.Integer)
+class InterviewResult(db.Model):
+    __tablename__ = 'interviewResult'
+    UID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    answer = db.Column(db.Text)
+    grade = db.Column(db.Integer, nullable=False)
+    evaluation = db.Column(db.Text, nullable=False)
+    status = db.Column(db.Integer, nullable=False)
+
+class Application(db.Model):
+    __tablename__ = 'application'
+    UID = db.Column(db.Integer, db.ForeignKey('user.UID'), primary_key=True)
+    PID = db.Column(db.Integer, db.ForeignKey('position.PID'), primary_key=True)
+    salary = db.Column(db.Integer)
--- App/views.py
+++ App/views.py
@@ -1,9 +1,41 @@
+from flask import Blueprint, request, redirect, flash, url_for, render_template
 
-from flask import Blueprint
+from .forms import RegisterForm, LoginForm
 from .models import *
 
 blue = Blueprint('user',__name__)
 
-@blue.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@blue.route('/', methods=['GET', 'POST'])  # 直接将根URL指向login函数
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = form.signInUsernameField.data
+        password = form.signInPasswordField.data
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('user.hello_world'))  # Redirect to a protected page or dashboard
+        else:
+            flash('Login failed. Check your username and password.', 'danger')
+
+    return render_template('login.html', form=form)
+
+@blue.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        email = form.signUpEmailField.data
+        username = form.signUpUsernameField.data
+        password = form.signUpPasswordField.data
+
+        new_user = User(email=email, username=username,
+                        password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registration successful! Please login.', 'success')
+        return redirect(url_for('user.login'))
+
+    return render_template('register.html', form=form)
